9d0d451cfecee6201fe9b1a94c6b42656734f521
bhthng098 <41706927+beanutbuttr@users.noreply.github.com>
2025-08-17 23:13:55 -0400

    upgrading and rebuilding node

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+puCQ0uMnIwIvtlKA2eKoARFJeLUutizAcrw2PJtnDU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
